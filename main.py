eff8fafae64d90ca0e6cb93c0090524c34c51a47
Kirill Temerev <64499601+Temerev-KS@users.noreply.github.com>
2022-01-08 01:54:44 +0300

    some test steps

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-print('i')
+from PIL import Image
 
+
+def func():
+    img = Image.open('img/dogo.jpeg')
+    print(img.format)
+    print(img.size)
+    print(img.mode)
+    img.show()
+    
+    
 if __name__ == '__main__':
-    print('o')
+    func()
     
