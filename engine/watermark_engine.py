2c027d6fe5b75b02ef854507662bb5e49ac9f432
Kirill Temerev <64499601+Temerev-KS@users.noreply.github.com>
2022-01-10 22:57:31 +0300

    create_watermark in progress

--- engine/watermark_engine.py
+++ engine/watermark_engine.py
@@ -1,9 +1,14 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont, ImageChops
 
 
 class WatermarkEngine:
     def __init__(self):
+        self._font_list = {
+            'OpenSans': '../fonts/ttf/Open_Sans/static/OpenSans/OpenSans-Regular.ttf',
+            'Caveat': '../fonts/ttf/Caveat/static/Caveat-Regular.ttf',
+        }
         self._current_image_obj = None
+        self._current_marked_image_obj = None
         self._current_image_name = None  # path with filename in one str
         self._current_image_path = None  # TODO: MAY BE WILL NOT NEED THAT IN THE FUTURE DEVELOPMENT
         self._current_image_size = None
@@ -13,11 +18,27 @@ class WatermarkEngine:
         # TODO: create variables for parameters
         pass
 
-    def apply_watermark(self, image_obj: Image):
+    def apply_watermark(
+            self, image_obj: Image,
+            watermark_content: str = "KT",
+            font_size: int = 150,
+            font: str = 'OpenSans',
+            margin_horizontal: int = 30,
+            margin_vertical: int = 30,
+            side: str = 'bottom',
+            color: str = 'WHITE',
+            opacity: int = '30',
+            across: bool = False
+    ):
         # TODO: Collect all methods here
         self._current_image_obj = image_obj
         self._gather_info()
-        pass
+        self._create_watermark(
+            font=self._font_list[font],
+            size=font_size,
+            content=watermark_content,
+        )
+        return self._current_marked_image_obj
     
     def _check_values(self):
         # TODO: Check if image exists
@@ -35,12 +56,31 @@ class WatermarkEngine:
         self._current_image_colorspace = self._current_image_obj.mode
     
     def _calculate_placement(self):
+        # https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html#PIL.ImageDraw.ImageDraw.textbbox
         # TODO: Create a mechanism that will output exactly where to put watermark
         # TODO: based on parameters like (width and height), watermark size, font, image size
         
         pass
     
-    def _create_watermark(self):
+    def _create_watermark(self, font, size, content):
+        # TODO: Look into https://pythonhosted.org/blend_modes/
+        # TODO: Also this !!!! more important https://colab.research.google.com/drive/1CJy_vzoOcQdomINNMz1TyjXLGsZLZzwF?usp=sharing
+        
+        # Initiate font class
+        font = ImageFont.truetype(font, size)
+        # Create new image the size of the _current_image
+        watermark_image = Image.new(
+            self._current_image_colorspace,
+            (self._current_image_width, self._current_image_height),
+            "white"
+        )
+        # Initiate class that will rasterize font on to the image
+        draw_engine = ImageDraw.Draw(watermark_image)
+        # Draw letters
+        draw_engine.text((self._current_image_width, self._current_image_height), content, fill="black", anchor="rd", font=font)
+        self._current_marked_image_obj = ImageChops.multiply(self._current_image_obj, watermark_image)
+    # TODO: Translate chosen side to anchor values https://pillow.readthedocs.io/en/stable/handbook/text-anchors.html
+        
         # TODO: Using predetermined parameters call for method to actually apply watermark
         pass
     
@@ -65,7 +105,6 @@ if __name__ == '__main__':
     test_img = dummy_file_func()
     
     # TODO: DO STUFF WITH IMAGE HERE
-    engine.apply_watermark(test_img)
     
-    # dummy_save_file_func()
+    dummy_save_file_func(engine.apply_watermark(test_img))
     pass
