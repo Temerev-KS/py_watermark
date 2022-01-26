7ff7047cef74f79728a7e48bf4a62eca4a4f9cd8
Kirill Temerev <64499601+Temerev-KS@users.noreply.github.com>
2022-01-26 20:14:06 +0300

    FileFeeder now returns path to the file and file name with extension

--- file_feeder/file_feeder.py
+++ file_feeder/file_feeder.py
@@ -12,21 +12,33 @@ class FileFeeder:
             for extension in self._file_types:
                 for image_file_path in Path(directory).rglob(f'*.{extension}'):
                     # look for any Jpeg file in folder including subfolders, if found - append its path to the list
-                    self._images_files_path.append(str(image_file_path))
+                    self._images_files_path.append(
+                        {'name': image_file_path.name, 'path': str(image_file_path)}
+                    )
                     
         elif include_subfolders is False:
             for extension in self._file_types:
                 for image_file_path in Path(directory).glob(f'*.{extension}'):
                     # look for any Jpeg file only in specified folder, if found - append its path to the list
-                    self._images_files_path.append(str(image_file_path))
+                    self._images_files_path.append(
+                        {'name': image_file_path.name, 'path': str(image_file_path)}
+                    )
         else:
             raise ValueError(f'Include_subfolders only accepts True or False, but {include_subfolders} was provided.')
     
     def process_file(self, file: str, *args, **kwargs):
         if (type(file) == tuple or type(file) == list) and len(file) >= 2:
-            self._images_files_path = [image_file_path for image_file_path in file]
+            # self._images_files_path = [image_file_path for image_file_path in file]
+            for item in file:
+                image_file_path = Path(item)
+                self._images_files_path.append(
+                    {'name': image_file_path.name, 'path': str(image_file_path)}
+                )
         else:
-            self._images_files_path.append(file)
+            image_file_path = Path(file)
+            self._images_files_path.append(
+                {'name': image_file_path.name, 'path': str(image_file_path)}
+            )
     
     def get_files(self):
         return self._images_files_path
@@ -55,8 +67,7 @@ if __name__ == '__main__':
         print(type(test_obj._images_files_path))
         for i in test_obj._images_files_path:
             print(i)
-        print(test_obj._images_files_path)
     
-    test_dir()
+    # test_dir()
     # test_without_subdir()
     # test_file()
