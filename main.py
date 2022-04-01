d1686534a876380eb8db957b03c3e9ff1011799e
Kirill Temerev <64499601+Temerev-KS@users.noreply.github.com>
2022-04-01 17:42:33 +0400

    Refreshed project development

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def full_cycle_test():
     file_feeder = FileFeeder()
     engine = WatermarkEngine()
     exporter = Exporter()
-    exporter.set_params(same_as_source=False, destination=r'C:\python_projects\py_watermark\export_img')
-    file_feeder.process_directory(r'C:\python_projects\py_watermark\img')
+    exporter.set_params(same_as_source=False, destination=r'export_img')
+    file_feeder.process_directory(r'img')
+    # file_feeder.process_file(r'img/dogo.jpeg')
     for image in file_feeder.get_files():
         final_image = engine.apply_watermark(image)
         exporter.export_file(final_image)
